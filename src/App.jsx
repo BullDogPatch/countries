97573b1d70c85232ec58a2d473e77ca031675426
Craig Clayton <craig__clayton@outlook.com>
2025-04-23 15:03:17 +0100

    add input to filter counties

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,21 @@ function App() {
     fetchCountries();
   }, []);
 
+  const filtered = query
+    ? countries.filter((country) =>
+        country.name.common.toLowerCase().includes(query)
+      )
+    : [];
+
   return (
     <>
-      {countries.map((country, i) => (
+      <input
+        type='text'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className='bg-gray-100 text-gray-900 w-1/2 p-2 border border-gray-300 focus:outline-none focus:border-amber-400 rounded'
+      />
+      {filtered.map((country, i) => (
         <Country key={country + i} country={country} />
       ))}
     </>
