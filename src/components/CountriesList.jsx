14e64891258e61c6f3a505b653a4c428548cf488
Craig Clayton <craig__clayton@outlook.com>
2025-04-23 16:08:46 +0100

    add show button to show country detail in the countries list

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import Country from './Country';
+
 const CountriesList = ({ country }) => {
+  const [show, setShow] = useState(false);
   return (
     <>
-      <p>{country.name.common}</p>
+      <div className='flex justify-center items-center m-2'>
+        <p className='mr-2'>{country.name.common}</p>
+        <button
+          className='cursor-pointer ml-2 rounded-md text-black bg-amber-200 p-1'
+          onClick={() => setShow((s) => !s)}
+        >
+          {show ? 'hide' : 'show'}
+        </button>
+      </div>
+      {show && <Country country={country} />}
     </>
   );
 };
